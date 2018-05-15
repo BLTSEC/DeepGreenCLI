57f9727d15a23f61b4777f58f1ebb0a0b54e9b8d
BLTSEC <bltsec.1337@gmail.com>
2018-05-15 11:54:01 -0500

    Go Go Gadget

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"bufio"
 	"regexp"
 	"os/exec"
+	"sync"
 )
 
 func removeDuplicates(elements []string) []string {
@@ -31,6 +32,9 @@ func removeDuplicates(elements []string) []string {
 }
 
 func printEvents(re *regexp.Regexp) {
+
+	var wg = sync.WaitGroup{}
+
 	// If the file doesn't exist, create it, or append to the file
 	f, err := os.OpenFile("/home/blt15b/win-events-results", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
 	if err != nil {
@@ -45,71 +49,83 @@ func printEvents(re *regexp.Regexp) {
 	logs, _ := filepath.Glob("/log/" + todayParent + "/" + todayChild + "/" + "snare*")
 
 	for _, winLog := range logs {
-		file, err := os.Open(winLog)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer file.Close()
 
-		scanner := bufio.NewScanner(file)
-
-		for scanner.Scan() {
-			reMatch := re.FindStringSubmatch(scanner.Text())
-			if reMatch == nil {
-				continue
+		wg.Add(1)
+		go func(winLog string) {
+			file, err := os.Open(winLog)
+			if err != nil {
+				log.Fatal(err)
 			}
+			defer file.Close()
 
-			eventID := reMatch[1]
+			scanner := bufio.NewScanner(file)
 
-			if strings.Contains(eventID, "4663") {
-				match4663 := re.FindStringSubmatch(scanner.Text())
-				server := strings.Replace(match4663[2], "Success Audit\t", "", -1)
-				user := strings.Replace(match4663[3], "Account Name: ", "", -1)
-				object := strings.Replace(match4663[4], "Object Name: ", "", -1)
-				process := strings.Replace(match4663[6], "Process Name: ", "", -1)
-				preUniqResult = append(preUniqResult, eventID+", "+server+", "+user+", "+process+", "+object)
-				continue
-			}
-
-			if strings.Contains(eventID, "4722") || strings.Contains(eventID, "4725") {
-				match4722m4725 := re.FindStringSubmatch(scanner.Text())
-				user := strings.Replace(match4722m4725[3], "ACULOCAL\\", "", -1)
-				admin := match4722m4725[6]
-				if strings.Contains(user, admin) {
+			for scanner.Scan() {
+				reMatch := re.FindStringSubmatch(scanner.Text())
+				if reMatch == nil {
 					continue
 				}
-				preUniqResult = append(preUniqResult, eventID+", "+user+", "+admin)
-				continue
-			}
 
-			if strings.Contains(eventID, "4720") {
-				matchre4720 := re.FindStringSubmatch(scanner.Text())
-				atrribs := matchre4720[3]
-				preUniqResult = append(preUniqResult, eventID+", "+atrribs)
-				continue
-			}
+				eventID := reMatch[1]
 
-			if strings.Contains(eventID, "4688") {
-				matchre4688 := re.FindStringSubmatch(scanner.Text())
-				server1 := matchre4688[3]
-				user1 := matchre4688[4]
-				user2 := matchre4688[5]
-				server2 := matchre4688[6]
-				process := matchre4688[7]
-				tokenName := matchre4688[8]
-				tokenLevel := matchre4688[9]
-				creatorProcess := matchre4688[10]
-				if strings.Contains(creatorProcess, "0x") {
-					preUniqResult = append(preUniqResult, eventID+", "+server1+", "+user1+", "+user2+", "+server2+", "+process+", "+tokenName+" "+tokenLevel)
-				} else {
-					preUniqResult = append(preUniqResult, eventID+", "+server1+", "+user1+", "+user2+", "+server2+", "+process+", "+creatorProcess)
+				// 4663: An attempt was made to access an object
+				if strings.Contains(eventID, "4663") {
+					eventID := strings.Replace(eventID, "	Microsoft-Windows-Security-Auditing", "", -1)
+					server := strings.Replace(reMatch[2], "Success Audit\t", "", -1)
+					user := strings.Replace(reMatch[3], "Account Name: ", "", -1)
+					object := strings.Replace(reMatch[4], "Object Name: ", "", -1)
+					process := strings.Replace(reMatch[6], "Process Name: ", "", -1)
+					preUniqResult = append(preUniqResult, eventID+", "+server+", "+user+", "+process+", "+object)
+					continue
+				}
+
+				// 4722: A user account was enabled
+				// 4725: A user account was disabled
+				if strings.Contains(eventID, "4722") || strings.Contains(eventID, "4725") {
+					user := strings.Replace(strings.Replace(reMatch[3], "ACULOCAL\\", "", -1), " ", "", -1)
+					admin := strings.Replace(reMatch[6], " ", "", -1)
+					if user != admin {
+						preUniqResult = append(preUniqResult, eventID+", "+user+", "+admin)
+					}
+					continue
+				}
+
+				// 4720: A user account was created
+				if strings.Contains(eventID, "4720") {
+					atrribs := reMatch[3]
+					preUniqResult = append(preUniqResult, eventID+", "+atrribs)
+					continue
+				}
+
+				// 4688: A new process has been created
+				if eventID == "4688" {
+					server1 := reMatch[3]
+					user1 := reMatch[4]
+					user2 := reMatch[5]
+					server2 := reMatch[6]
+					process := reMatch[7]
+					tokenName := reMatch[8]
+					tokenLevel := reMatch[9]
+					creatorProcess := reMatch[10]
+					if strings.Contains(user1, "$") {
+						continue
+					}
+					if strings.Contains(creatorProcess, "0x") {
+						preUniqResult = append(preUniqResult, eventID+", "+server1+", "+user1+", "+user2+", "+server2+", "+process+", "+tokenName+" "+tokenLevel)
+					} else {
+						preUniqResult = append(preUniqResult, eventID+", "+server1+", "+user1+", "+user2+", "+server2+", "+process+", "+creatorProcess)
+					}
 				}
 			}
-		}
-		if err := scanner.Err(); err != nil {
-			log.Fatal(err)
-		}
+			if err := scanner.Err(); err != nil {
+				log.Fatal(err)
+			}
+
+			wg.Done()
+		}(winLog)
 	}
+
+	wg.Wait()
 
 	result := removeDuplicates(preUniqResult)
 	for _, event := range result {
@@ -126,7 +142,7 @@ func main() {
 	re4663 := regexp.MustCompile(`(?m)(?P<eventid>4663	Microsoft-Windows-Security-Auditing.*?).+(?P<server>Success Audit\s.*?\S+).+(?P<subject>Account Name:\s.*?\S+).+(?P<object>Object Name:\s.*\S).+(\sHandle).+(?P<process>Process Name:\s.*\S).+(\sAccess Request)`)
 	re4722re4725 := regexp.MustCompile(`(?m)(4722|4725).*(	Microsoft-Windows-Security-Auditing.*?\t)(.*\S).*(N/A).*(Account Name:  )(.*)(Account Domain:  ACULOCAL   Logon ID:)`)
 	re4720 := regexp.MustCompile(`(?m)(?P<eventid>4720)(\tMicrosoft-Windows-Security-Auditing.*?)Account Name:.*?(Account Name:.*)(Additional Information:.*)`)
-	re4688 := regexp.MustCompile(`(?m)(?P<one>4688)(\tMicrosoft-Windows-Security-Auditing.*?)Audit\s(?P<three>[^ \s]*).*?Account Name:\s\s(?P<four>[^ ]*)\s{3}Account Domain:\s{2}ACULOCAL.*Account Name:\s{2}(?P<five>[^ ]*)\s{3}Account Domain:\s{2}(?P<six>[^ ]*).*New Process Name:\s(?P<seven>[^ ]*).*?Token Elevation Type:\s(?P<eight>[^ ]*).(?P<nine>[^ ]*).*Creator Process\s\w*:\s(?P<ten>[^ ]*)`)
+	re4688 := regexp.MustCompile(`(?m)(?P<one>4688)(\tMicrosoft-Windows-Security-Auditing.*?)Audit\s(?P<three>[^ \s]*).*?Account Name:\s\s(?P<four>[^ ]*)\s{3}Account Domain:\s{2}ACULOCAL.*Account Name:\s{2}(?P<five>[^ ]*)\s{3}Account Domain:\s{2}(?P<six>.*).*\s{3}Logon ID:.*New Process Name:\s(?P<seven>.*).*?\s{3}Token Elevation Type:\s(?P<eight>[^ ]*).(?P<nine>[^ ]*).*Creator Process\s\w*:\s(?P<ten>.*).*\s{3}Process`)
 
 	printEvents(re4663)
 	printEvents(re4722re4725)
